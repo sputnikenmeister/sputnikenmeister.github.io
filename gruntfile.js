6aaab1959ee12b44408b59befd5c79a2680c4683
Pablo Canillas <pablo@canillas.name>
2015-01-13 16:00:02 +0100

    ---

--- gruntfile.js
+++ gruntfile.js
@@ -15,21 +15,36 @@ module.exports = function (grunt) {
 			options: { url: 'http://folio.localhost/workspace/assets/css/folio.css' },
 			dest: 'workspace/assets/css/folio.css'
 		},
-		// vendor: {
-		// 	options: { url: 'http://folio.localhost/workspace/assets/js/folio-vendor.js' },
-		// 	dest: 'build/folio-vendor.js'
-		// },
-		// client: {
-		// 	options: { url: 'http://folio.localhost/workspace/assets/js/folio-client.js' },
-		// 	dest: 'build/folio-client.js'
-		// },
+		data: {
+			options: { url: 'http://folio.localhost/json' },
+			dest: 'workspace/assets/js/folio-data.js'
+		},
+		vendor: {
+			options: { url: 'http://folio.localhost/workspace/assets/js/folio-vendor.js' },
+			dest: 'workspace/assets/js/folio-vendor.js'
+		},
+		client: {
+			options: { url: 'http://folio.localhost/workspace/assets/js/folio-client.js' },
+			dest: 'workspace/assets/js/folio-client.js'
+		},
 	});
 	
 	grunt.loadNpmTasks('grunt-string-replace');
 	grunt.config("string-replace", {
+		"data-url": {
+			files: {
+				'./': ['index.html']
+			},
+			options: {
+				replacements: [{
+					pattern: /http:\/\/folio.localhost\/json/,
+					replacement: '/workspace/assets/js/folio-data.js'
+				}]
+			}
+		},
 		"root-url": {
 			files: {
-				'./': ['index.html','workspace/assets/css/folio.css']
+				'./': ['index.html','workspace/assets/css/folio.css','workspace/assets/js/folio-data.js']
 			},
 			options: {
 				replacements: [{
@@ -41,6 +56,6 @@ module.exports = function (grunt) {
 	});
 	
 	grunt.registerTask("http-folio-get", ["http:index", "http:styles"]);
-	grunt.registerTask("build", ["http", "string-replace:root-url"]);
+	grunt.registerTask("build", ["http", "string-replace"]);
 	grunt.registerTask("default", ["build"]);
 };
